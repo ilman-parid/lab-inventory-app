a86bc6dec85723078263832ff475d96c994b97fb
Ilman Parid Hakim <faridhakiem8@gmail.com>
2026-07-09 19:33:31 +0700

    Delete lab-inventory-app directory

--- lab-inventory-app/backend/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../config/db');
-const Barang = require('./Barang');
-const Peminjaman = require('./Peminjaman');
-
-module.exports = {
-  sequelize,
-  Barang,
-  Peminjaman,
-};
